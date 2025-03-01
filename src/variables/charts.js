ba7320618483558d883bfde9a0a18968e7c78a83
JJ-IM <20231294@daejin.ac.kr>
2025-03-01 13:41:59 +0000

    임시 업로드 (로컬)

---
+++ src/variables/charts.js
@@ -0,0 +1,174 @@
+export const barChartData = [
+  {
+    name: "Sales",
+    data: [330, 250, 110, 300, 490, 350, 270, 130, 2025],
+  },
+];
+
+export const barChartOptions = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+  },
+  tooltip: {
+    style: {
+      backgroundColor: "red",
+      fontSize: "12px",
+      fontFamily: undefined,
+    },
+    onDatasetHover: {
+      style: {
+        backgroundColor: "red",
+        fontSize: "12px",
+        fontFamily: undefined,
+      },
+    },
+    theme: "dark",
+  },
+  xaxis: {
+    categories: [
+      "전국",
+      "서울",
+      "경기",
+      "강원",
+      "경남",
+      "경북",
+      "충남",
+      "충북",
+      "부산",
+    ], // 변경된 부분
+    show: true, // 변경: false -> true
+    labels: {
+      show: true,
+      style: {
+        colors: "#fff",
+        fontSize: "12px",
+      },
+    },
+    axisBorder: {
+      show: false,
+    },
+    axisTicks: {
+      show: false,
+    },
+  },
+  yaxis: {
+    show: true,
+    color: "#fff",
+    labels: {
+      show: true,
+      style: {
+        colors: "#fff",
+        fontSize: "14px",
+      },
+    },
+  },
+  grid: {
+    show: false,
+  },
+  fill: {
+    colors: "#fff",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 8,
+      columnWidth: "12px",
+    },
+  },
+  responsive: [
+    {
+      breakpoint: 768,
+      options: {
+        plotOptions: {
+          bar: {
+            borderRadius: 0,
+          },
+        },
+      },
+    },
+  ],
+};
+
+export const lineChartData = [
+  {
+    name: "Mobile apps",
+    data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+  },
+  {
+    name: "Websites",
+    data: [30, 90, 40, 140, 290, 290, 340, 230, 400],
+  },
+];
+
+export const lineChartOptions = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+  },
+  tooltip: {
+    theme: "dark",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  xaxis: {
+    type: "datetime",
+    categories: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    labels: {
+      style: {
+        colors: "#c8cfca",
+        fontSize: "12px",
+      },
+    },
+  },
+  yaxis: {
+    labels: {
+      style: {
+        colors: "#c8cfca",
+        fontSize: "12px",
+      },
+    },
+  },
+  legend: {
+    show: false,
+  },
+  grid: {
+    strokeDashArray: 5,
+  },
+  fill: {
+    type: "gradient",
+    gradient: {
+      shade: "light",
+      type: "vertical",
+      shadeIntensity: 0.5,
+      gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
+      inverseColors: true,
+      opacityFrom: 0.8,
+      opacityTo: 0,
+      stops: [],
+    },
+    colors: ["#4FD1C5", "#2D3748"],
+  },
+  colors: ["#4FD1C5", "#2D3748"],
+};
