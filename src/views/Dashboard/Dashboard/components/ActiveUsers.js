104606cacfd79bda47a7d556be74cc7010333a9a
JJ-IM <20231294@daejin.ac.kr>
2025-03-06 21:39:06 +0000

    이쁘다다

--- src/views/Dashboard/Dashboard/components/ActiveUsers.js
+++ src/views/Dashboard/Dashboard/components/ActiveUsers.js
@@ -1,5 +1,5 @@
 // Chakra imports
-import { Flex, SimpleGrid, Text, useColorModeValue } from "@chakra-ui/react";
+import { Flex, SimpleGrid, Text, useColorModeValue, Box } from "@chakra-ui/react"; // Box 추가
 // Custom components
 import Card from "components/Card/Card.js";
 import CardBody from "components/Card/CardBody.js";
@@ -10,12 +10,32 @@ import {
   StatsIcon,
   WalletIcon,
 } from "components/Icons/Icons.js";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChartStatistics from "./ChartStatistics";
+import { ReactComponent as WaterIcon } from "components/Icons/water.svg"; // 추가된 임포트
 
-const ActiveUsers = ({ title, percentage, chart }) => {
+const ActiveUsers = ({ chart }) => {
   const iconBoxInside = useColorModeValue("white", "white");
   const textColor = useColorModeValue("gray.700", "white");
+  const [nationalData, setNationalData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://203.237.81.27:8000/dashboard-api/national-average", { credentials: "include" })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("national-average response:", data);
+        setNationalData(data.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  // 만약 데이터가 없으면 기본값 사용
+  const lastWeekChange = nationalData ? nationalData[0][0] : 0;
+  const fuelValues = nationalData ? nationalData[1] : [0, 0, 0, 0];
+  // fuel 종류: Diesel, Gas, Premium, Kero
+  const fuelTitles = ["Diesel", "Gas", "Premium", "Kero"];
+  const fuelColors = ["#123E01", "#E4960E", "#B30709", "#767676"];
+
   return (
     <Card p='16px'>
       <CardBody>
@@ -23,43 +43,32 @@ const ActiveUsers = ({ title, percentage, chart }) => {
           {chart}
           <Flex direction='column' mt='24px' mb='36px' alignSelf='flex-start'>
             <Text fontSize='lg' color={textColor} fontWeight='bold' mb='6px'>
-              {title}
+              전국 유가
             </Text>
             <Text fontSize='md' fontWeight='medium' color='gray.400'>
-              <Text
-                as='span'
-                color={percentage > 0 ? "green.400" : "red.400"}
-                fontWeight='bold'>
-                {percentage > 0 ? `+${percentage}%` : `-${percentage}%`}
+              <Text as='span' fontWeight='bold' color={lastWeekChange > 0 ? "green.400" : "red.400"}>
+                {lastWeekChange}
               </Text>{" "}
               than last week
             </Text>
           </Flex>
           <SimpleGrid gap={{ sm: "12px" }} columns={4}>
-            <ChartStatistics
-              title={"Users"}
-              amount={"32,984"}
-              percentage={20}
-              icon={<WalletIcon h={"15px"} w={"15px"} color={iconBoxInside} />}
-            />
-            <ChartStatistics
-              title={"Clicks"}
-              amount={"2.42m"}
-              percentage={80}
-              icon={<RocketIcon h={"15px"} w={"15px"} color={iconBoxInside} />}
-            />
-            <ChartStatistics
-              title={"Sales"}
-              amount={"2,400$"}
-              percentage={30}
-              icon={<CartIcon h={"15px"} w={"15px"} color={iconBoxInside} />}
-            />
-            <ChartStatistics
-              title={"Items"}
-              amount={"320"}
-              percentage={40}
-              icon={<StatsIcon h={"15px"} w={"15px"} color={iconBoxInside} />}
-            />
+            {fuelTitles.map((title, idx) => (
+              <ChartStatistics
+                key={title}
+                title={title}
+                amount={fuelValues[idx]}
+                percentage={0}
+                icon={
+                  <Box 
+                    as={WaterIcon} 
+                    width="15px" 
+                    height="15px" 
+                    sx={{ fill: `${fuelColors[idx]} !important` }}
+                  />
+                }
+              />
+            ))}
           </SimpleGrid>
         </Flex>
       </CardBody>
--- src/views/Dashboard/Dashboard/components/ChartStatistics.js
+++ src/views/Dashboard/Dashboard/components/ChartStatistics.js
@@ -1,12 +1,11 @@
-import { Flex, Progress, Text, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
 import IconBox from "components/Icons/IconBox";
 import React from "react";
 
-const ChartStatistics = ({ title, amount, icon, percentage }) => {
+const ChartStatistics = ({ title, amount, icon }) => {
   const iconTeal = useColorModeValue("teal.300", "teal.300");
   const iconBoxInside = useColorModeValue("white", "white");
   const textColor = useColorModeValue("gray.700", "white");
-  const overlayRef = React.useRef();
   return (
     <Flex direction='column'>
       <Flex alignItems='center'>
@@ -20,12 +19,6 @@ const ChartStatistics = ({ title, amount, icon, percentage }) => {
       <Text fontSize='lg' color={textColor} fontWeight='bold' mb='6px' my='6px'>
         {amount}
       </Text>
-      <Progress
-        colorScheme='teal'
-        borderRadius='12px'
-        h='5px'
-        value={percentage}
-      />
     </Flex>
   );
 };
